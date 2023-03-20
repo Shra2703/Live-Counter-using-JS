d41abb2d562d98b7504bb28389d89a53a0369a7e
Shraddha Goyal <shraddhagoyal800@gmail.com>
2023-03-20 18:59:28 +0530

    logic part and html part is completed

---
+++ index.js
@@ -0,0 +1,21 @@
+let counters = document.querySelectorAll('.counter');
+// console.log(counters);
+
+counters.forEach((counter) =>{
+    // console.log(counter);
+    counter.innerHTML = 0;
+
+    const updateCounter = () =>{
+       let targetCount = +counter.getAttribute('data-target');
+        // console.log(typeof(targetCount));
+        let startingCount = Number(counter.innerHTML);
+        let incr = targetCount / 100;
+
+        if(startingCount < targetCount){
+            counter.innerHTML = `${startingCount + incr}`
+            setTimeout(updateCounter, 10);
+        }
+    }
+
+    updateCounter();
+})
